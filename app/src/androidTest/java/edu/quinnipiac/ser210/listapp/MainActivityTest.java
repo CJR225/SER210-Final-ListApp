2136cd3d5fc74d90956d944032cbe7a47263ccd6
chrisrocco <cjrocco@quinnipiac.edu>
2022-05-05 13:03:51 -0400

    Testing

--- app/src/androidTest/java/edu/quinnipiac/ser210/listapp/MainActivityTest.java
+++ app/src/androidTest/java/edu/quinnipiac/ser210/listapp/MainActivityTest.java
@@ -2,10 +2,14 @@ package edu.quinnipiac.ser210.listapp;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
 
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.view.Gravity;
+
+import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -22,6 +26,15 @@ public class MainActivityTest {
     @Test
     public void onNavigationItemSelected() {
     onView(withId(R.id.newbutton)).perform(click());
+    }
+    @Test
+    public void onSupportNavigateUp() {
+
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open()); // Open Drawer
+
+        onView(withId(R.id.backgroundblue)).perform(click());
     }
     
 }
