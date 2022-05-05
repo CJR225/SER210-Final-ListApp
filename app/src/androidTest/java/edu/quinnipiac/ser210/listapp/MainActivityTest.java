6e85bb8ee99f446f3c2867ce517300429d574c2f
chrisrocco <cjrocco@quinnipiac.edu>
2022-05-05 00:28:00 -0400

    Testing

---
+++ app/src/androidTest/java/edu/quinnipiac/ser210/listapp/MainActivityTest.java
@@ -0,0 +1,27 @@
+package edu.quinnipiac.ser210.listapp;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void onNavigationItemSelected() {
+    onView(withId(R.id.newbutton)).perform(click());
+    }
+    
+}
